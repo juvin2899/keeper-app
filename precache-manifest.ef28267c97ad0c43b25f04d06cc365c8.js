a461b4bb44427f9dd423050ff9f091828f0bc9e7
juvin2899 <68566718+juvin2899@users.noreply.github.com>
2021-09-10 02:39:25 +0530

    Updates

---
+++ precache-manifest.ef28267c97ad0c43b25f04d06cc365c8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8857c79ebb7528cd5370c720458c53",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "e89784fdf5fd60939bfb",
+    "url": "/keeper-app/static/js/2.a4590682.chunk.js"
+  },
+  {
+    "revision": "e538897a23a0fdfbfd69",
+    "url": "/keeper-app/static/js/main.34d372a4.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
